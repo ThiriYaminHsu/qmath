2e8bbcdc27865939fd55f0f95fce5948b794d48e
Dmytro Fedoriaka <fedimser@uw.edu>
2025-11-29 16:36:04 +0000

    optimize controlled swap

--- qmath/utils/gates.py
+++ qmath/utils/gates.py
@@ -5,22 +5,16 @@ from typing import Optional
 
 def cnot(a: Qubits, b: Qubits, ctrl: Optional[Qubits] = None):
     assert len(a) == len(b) == 1
-    if ctrl is not None:
-        b.x(a | ctrl)
-    else:
-        b.x(a)
+    b.x(a | ctrl)
 
 
 def ccnot(a: Qubits, b: Qubits, c: Qubits, ctrl: Optional[Qubits] = None):
     assert len(a) == len(b) == len(c) == 1
-    if ctrl is not None:
-        c.x(a | b | ctrl)
-    else:
-        c.x(a | b)
+    c.x(a | b | ctrl)
 
 
 def controlled_swap(ctrl: Qubits, a: Qubits, b: Qubits):
     assert len(ctrl) == len(a) == len(b) == 1
-    ccnot(ctrl, a, b)
-    ccnot(ctrl, b, a)
-    ccnot(ctrl, a, b)
+    b.x(a)
+    a.x(b | ctrl)
+    b.x(a)
