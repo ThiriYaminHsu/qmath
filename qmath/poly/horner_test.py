efa4049b4036d23563be185e717e9924128a2b23
Dmytro Fedoriaka <fedimser@uw.edu>
2025-11-30 04:36:44 +0000

    skip slow tests

--- qmath/poly/horner_test.py
+++ qmath/poly/horner_test.py
@@ -1,18 +1,36 @@
 """Quantum algorithms for evaluating polynomials and polynomial approximations."""
 
+import os
 import random
 
 import numpy as np
+import pytest
 from psiqworkbench import QPU, QFixed
 
 from qmath.poly import HornerScheme
 
+RUN_SLOW_TESTS = os.getenv("RUN_SLOW_TESTS") == "1"
 
-def test_horner():
+
+@pytest.mark.parametrize("coefs", [[2], [-2, 3.5], [3.5, 2.5, -1]])
+def test_horner_linear(coefs: list[float]):
+    qpu = QPU(filters=[">>64bit>>", ">>bit-sim>>"])
+    qpu.reset(100)
+    hs = HornerScheme(coefs)
+    qx = QFixed(8, name="qx", radix=4, qpu=qpu)
+    x = 1.25
+    qx.write(x)
+    with hs.computed(qx):
+        result = hs.get_result_qreg().read()
+    assert result == np.polyval(coefs[::-1], x)
+
+
+@pytest.mark.skipif(not RUN_SLOW_TESTS, reason="slow test")
+def test_horner_random():
     qpu = QPU(filters=[">>64bit>>", ">>bit-sim>>"])
     qpu.reset(500)
     coefs = [5.1, -4.2, 0.8]
-    num_trials = 1
+    num_trials = 2
 
     for _ in range(num_trials):
         x = -10 + 20 * random.random()
